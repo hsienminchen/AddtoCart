f5679a140ce818d9d632426b9be61a9134273cf4
hsienminchen <hsienminchen@gmail.com>
2023-04-17 12:24:51 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,68 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+  databaseURL: "https://realtime-database-37287-default-rtdb.asia-southeast1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "shoppingList")
+
+const inputFieldEl = document.getElementById("input-field")
+const addButtonEl = document.getElementById("add-button")
+const shoppingListEl = document.getElementById("shopping-list")
+
+addButtonEl.addEventListener("click", function() {
+  let inputValue = inputFieldEl.value
+
+  push(shoppingListInDB, inputValue)
+
+  clearInputFieldEl()
+  
+})
+
+onValue(shoppingListInDB, function(snapshot) {
+
+  if (snapshot.exists()) {
+    let itemArray = Object.entries(snapshot.val())
+
+    clearShoppingLisrEl()
+
+    for(let i = 0; i < itemArray.length; i++) {
+      let currentItem = itemArray[i]
+      let currentItemId = currentItem[0]
+      let currentItemValue = currentItem[1]
+      
+      appendItemToShoppingListEl(currentItem)
+    }
+  } else {
+    shoppingListEl.innerHTML = "No items here... yet"
+  }
+
+})
+
+function clearShoppingLisrEl() {
+  shoppingListEl.innerHTML = ""
+}
+
+function clearInputFieldEl() {
+  inputFieldEl.value = ""
+}
+
+function appendItemToShoppingListEl(item) {
+  let itemID = item[0]
+  let itemValue = item[1]
+  
+  let newEl = document.createElement("li")
+
+  newEl.textContent = itemValue
+
+  newEl.addEventListener("click", function() {
+    let exectLocationOfItemInDB = ref(database, `shoppingList/${itemID}`)
+
+    remove(exectLocationOfItemInDB)
+  })
+
+  shoppingListEl.append(newEl)
+}
